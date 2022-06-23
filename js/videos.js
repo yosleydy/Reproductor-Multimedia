238da85b50501757014a313f9d721fc277ef5d50
yosleydy <yoslexio@gmail.com>
2022-06-23 13:13:29 -0400

    imagenes de los videos

--- js/videos.js
+++ js/videos.js
@@ -1,5 +1,58 @@
+let data = [
+    {
+        id:0,
+        titulo:'Evanescence',
+        video:'./assets/videos/Evanescence.mp4#t=0.5',
+        img:'./assets/img/evanescence.webp'
+    },
+    {
+        id:1,
+        titulo:'Aerosmith',
+        video:'./assets/videos/Aerosmith.mp4',
+        img:'./assets/img/aerosmith.jpg',
+    },
+    {
+        id:2,
+        titulo:'Gorillaz',
+        video:'./assets/videos/Gorillaz.mp4',
+        img:'./assets/img/Gorillaz.webp',
+    },
+    {
+        id:3,
+        titulo:'Linkin_Park',
+        video:'./assets/videos/Linkin_Park.mp4',
+        img:'./assets/img/Linkin_Park.jpg',
+    },
+]
+
+
+
+let ul = document.getElementById("lista_de_videos");
+let elementsLi = [];
+
+const source = document.getElementById("videoPrincipal")
+source.setAttribute('src',data[3]['video']);
+
+
+data.forEach(id => {
+    elementsLi.push(id.titulo)
+    const li = document.createElement('li')
+    li.setAttribute('title',id.titulo)
+    const videos = document.createElement('video')
+    videos.setAttribute('class','videos')
+    videos.setAttribute('id',data[id.id]['id'])
+    videos.setAttribute('src',data[id.id]['video'])
+    videos.setAttribute('poster',data[id.id]['img'])
+    li.appendChild(videos)
+    ul.appendChild(li)
+})
+
+
+
+
 function videoPlayer(){
-    const video = document.getElementById("video")
+    const video = document.getElementById("videoPrincipal")
+
     const videoContainer = document.getElementById("video-container")
     const play = document.getElementById("play")
     const control = document.getElementById("control")
@@ -7,18 +60,18 @@ function videoPlayer(){
     const forwardSeconds = document.getElementById("forwardSeconds")
     const fullScreen = document.getElementById("fullScreen")
 
-    let onFullscreen = false
+    let onFullscreen = false;
+    let duration;
 
-    let duration
+/*console.log('ghfgh');
+    const source = document.getElementById("principal")
+    source.setAttribute('src',data[0]['video']);*/
 
     video.removeAttribute("controls")
-
-    video.addEventListener("playing",()=>{
-        
+  /* video.addEventListener("playing",()=>{
     })
     video.addEventListener("pause",()=>{
-        
-    })
+    })*/
 
     video.addEventListener("loadeddata",(event)=>{
         duration = event.target.duration
@@ -27,20 +80,18 @@ function videoPlayer(){
     video.addEventListener("timeupdate",(event)=>{
         const percentage = (event.target.currentTime/duration)*100
         control.value = percentage
-    })
-
-    // on input se activa en cada step del range
+    });
     control.oninput = (event)=>{
         video.currentTime = (duration/100)*event.target.value
     }
 
- /*   control.addEventListener("mousemove",(event)=>{
-        console.log(event)
-    })*/
+    document.getElementById("0").addEventListener("mousemove",(event)=>{
+       alert('rfghf');
+    })
 
     backwardSeconds.onclick = () =>{
         video.currentTime = video.currentTime-1
-    }
+    };
     forwardSeconds.onclick = () =>{
         video.currentTime = video.currentTime+1
     }
@@ -53,9 +104,7 @@ function videoPlayer(){
             onFullscreen = true
             videoContainer.requestFullscreen()
         }
-    }
-        
-
+    };
     play.onclick = ()=>{
         if(video.paused){
             video.play()
@@ -63,6 +112,9 @@ function videoPlayer(){
             video.pause()
         }
     }
+
+    console.log(data);
 }
+
 
 videoPlayer();
